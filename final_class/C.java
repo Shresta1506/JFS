6b74e3711bc7a2cdc1842449e52609dcc9c8c551
Shresta1506 <165762374+Shresta1506@users.noreply.github.com>
2025-08-06 13:28:14 +0530

    Add files via upload

---
+++ final_class/C.java
@@ -0,0 +1,6 @@
+public class C {
+    public static void main(String[] args) {
+        A obj = new A();   // Instantiating class A
+        obj.display();     // Calling method from class A
+    }
+}
